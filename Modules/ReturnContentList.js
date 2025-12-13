a48998c8fc871c1f51104cfac6c6e963c842a0ea
MasterClassOnRightTrack <vangzimin125@gmail.com>
2025-12-13 19:07:16 +0100

    Update ReturnContentList.js
    fixed link

--- Modules/ReturnContentList.js
+++ Modules/ReturnContentList.js
@@ -1,4 +1,5 @@
 export const contentList = {
-    Staza: "https://raw.githubusercontent.com/MasterClassOnRightTrack/filmtracktexture/refs/heads/main/film%20movements.json",
+    Staza: "https://masterclassonrighttrack.github.io/filmtracktexture/film%20movements.json",
     TeksturaTrake: "https://raw.githubusercontent.com/MasterClassOnRightTrack/filmtracktexture/main/Film%20Track%20Optimized.png"
 };
+
